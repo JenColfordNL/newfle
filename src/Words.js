ce335d4b52ce8aff4c72523edf442cc975b7584f
JenColfordNL <jennifer.colford@keyin.com>
2023-05-09 11:38:46 -0230

    functioning but not quite there

---
+++ src/Words.js
@@ -0,0 +1,24 @@
+import wordBank from "./wordle-bank.txt";
+
+export const boardDefault = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  let todaysWord;
+  let nlwordle = 174;
+  await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArr = result.split("\n");
+      todaysWord = wordArr[Math.floor(Math.random() * nlwordle)];
+      wordSet = new Set(wordArr);
+    });
+  return { wordSet, todaysWord };
+};
